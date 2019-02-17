2a5d9bc0fcd78d09610a64db30e5988137895107
Riley-Brown <riley@riley.gg>
2019-02-17 01:30:27 -0500

    Added album styles

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,7 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h3>Albums</h3>
+  @if(count($albums) > 0)
+      <div class="album">
+        @foreach($albums as $album)
+          <div>
+            <a href="/albums/{{$album->id}}">
+            <img src="storage/album_covers/{{$album->cover_image}}" alt="">
+          </a>
+          <br>
+          <h4>{{$album->name}}</h4>
+        </div>
+        @endforeach
+      </div>
+  @endif
 
 @endsection
