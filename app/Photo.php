9a1a3a29d000e092f0a112651ecb56d3a71f460e
Riley-Brown <riley@riley.gg>
2019-02-16 21:56:35 -0500

    Created relationship between album and photo models

--- app/Photo.php
+++ app/Photo.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    //
+    // fillable fields from DB
+    protected $fillable = array('album_id', 'description', 'photo', 'title', 'size');
+
+    public function album() {
+        // creates relationship with album
+        return $this->belongsTo('App\Album');
+    }
 }
