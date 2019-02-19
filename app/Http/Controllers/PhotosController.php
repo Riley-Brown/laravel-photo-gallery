8b6883c8908e77fbf8406e438e86b0b31535eca3
Riley-Brown <riley@riley.gg>
2019-02-18 23:03:21 -0500

    Added functionality to upload photos to an album

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -3,10 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Photo;
 
 class PhotosController extends Controller
 {
     public function create($album_id) {
         return view('photos.create')->with('album_id', $album_id);
+    }
+    
+    public function store(Request $request) {
+        $this->validate($request, [
+            'title' => 'required',
+            'photo' => 'image|max:1999'
+        ]);
+ 
+         // gets filename with extension
+         $fileNameWithExt =  $request->file('photo')->getClientOriginalName();
+ 
+         // returns filename
+         $fileName = pathinfo($fileNameWithExt, PATHINFO_FILENAME);
+ 
+         // get extension
+         $extension = $request->file('photo')->getClientOriginalExtension();
+ 
+         $fileNameToStore = $fileName.'_'.time().'.'.$extension;
+ 
+         $path = $request->file('photo')->storeAs('public/photos/'.$request->input('album_id'), $fileNameToStore);
+ 
+         // upload photo to album
+         $photo = new Photo;
+         
+         // fields from model
+         $photo->title = $request->input('title');
+         $photo->album_id = $request->input('album_id');
+         $photo->description = $request->input('description');
+         $photo->photo = $fileNameToStore;
+         $photo->size = $request->file('photo')->getClientSize();
+ 
+         // save to DB
+         $photo->save();
+ 
+         return redirect('/albums/'.$request->input('album_id'))->with('success', 'Photo Uploaded');
     }
 }
