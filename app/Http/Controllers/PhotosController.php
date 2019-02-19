e0b8d411d76cbc9a24c50e70259795eb31ccb42c
Riley-Brown <riley@riley.gg>
2019-02-18 21:41:22 -0500

    Added method to return a view with album ID

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class PhotosController extends Controller
 {
-    //
+    public function create($album_id) {
+        return view('photos.create')->with('album_id', $album_id);
+    }
 }
