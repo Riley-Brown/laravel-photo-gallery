2b943abf268883c3bbe5cc15713ec44d6f575f90
Riley-Brown <riley@riley.gg>
2019-02-18 23:17:26 -0500

    added for loop to view to display each photo in an album

--- resources/views/albums/show.blade.php
+++ resources/views/albums/show.blade.php
@@ -5,4 +5,19 @@
   <a class="button secondary" href="/">Go Back</a>
   <a href="/photos/create/{{$album->id}}" class="button">Upload Photo to Album</a>
   <hr>
+  @if(count($album->photos) > 0)
+      <div class="album">
+        @foreach($album->photos as $photo)
+          <div>
+            <a href="/photos/{{$photo->id}}">
+            <img src="/storage/photos/{{$photo->album_id}}/{{$photo->photo}}" alt="{{$photo->title}}">
+          </a>
+          <br>
+          <h4>{{$photo->title}}</h4>
+        </div>
+        @endforeach
+      </div>
+  @endif
+
+
 @endsection
